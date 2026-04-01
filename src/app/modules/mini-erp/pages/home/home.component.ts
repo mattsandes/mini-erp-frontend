845cda9723318b7a9704723758686e9391121708
Mateus Sandes <mateussandes@hotmail.com.br>
2026-04-01 10:09:10 -0300

    Refatorado a pagina da homepage

--- src/app/modules/mini-erp/pages/home/home.component.ts
+++ src/app/modules/mini-erp/pages/home/home.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { ContentComponent } from '../../components/content/content.component';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { HeaderComponent } from '../../components/header/header.component';
+import { IProducts } from '../../interfaces/IProducts';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    MatCardModule,
+    MatGridList,
     HeaderComponent,
-    ContentComponent
-  ],
+    MatGridTile
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public products: IProducts[] = [];
+  constructor(private service: ProductService) {}
+
+  ngOnInit(): void {
+      this.service.getAllProducts().subscribe({
+        next: (data) => this.products = data,
+        error: (error) => console.log(error),
+        complete: () => console.log("Request finished")
+      })
+  }
 }
