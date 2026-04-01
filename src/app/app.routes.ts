bbae5c97cf05cde16f2fbcffe64b4c2968e96250
Mateus Sandes <mateussandes@hotmail.com.br>
2026-04-01 10:06:34 -0300

    ADicionadas novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { CreateProductComponent } from './modules/mini-erp/pages/create-product/create-product.component';
 import { HomeComponent } from './modules/mini-erp/pages/home/home.component';
+import { ProductsComponent } from './modules/mini-erp/pages/products/products.component';
 
 export const routes: Routes = [
   {
     title: 'Homepage', component: HomeComponent, path: ''
   },
   {
-    title: 'Create Product', component: CreateProductComponent, path: 'create-product'
+    title: 'Products Page', component: ProductsComponent, path: 'products'
   }
 ];
