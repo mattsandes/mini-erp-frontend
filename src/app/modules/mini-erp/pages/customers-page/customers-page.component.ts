3f0b654e1e05ec0b23314530e80121ce7367fcf0
Mateus Sandes <mateussandes@hotmail.com.br>
2026-04-06 15:35:58 -0300

    Criado o filtro de clientes feito pelo front

--- src/app/modules/mini-erp/pages/customers-page/customers-page.component.ts
+++ src/app/modules/mini-erp/pages/customers-page/customers-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CUSTOMER_COLUMNS } from '../../configs/customer-table.columns';
 import { ICustomers } from '../../interfaces/customers/ICustomers';
@@ -10,22 +12,35 @@ import { ClientService } from '../../services/client.service';
   standalone: true,
   imports: [
     MatTableModule,
-    HeaderComponent
-  ],
+    HeaderComponent,
+    MatFormField,
+    MatLabel,
+    MatInput
+],
   templateUrl: './customers-page.component.html',
   styleUrl: './customers-page.component.scss'
 })
 export class CustomersPageComponent implements OnInit {
 
+  dataSource = new MatTableDataSource<ICustomers>([]);
   protected displayedColumns = CUSTOMER_COLUMNS;
   public customers: ICustomers[] = [];
   constructor(private service: ClientService) {}
 
   ngOnInit(): void {
     this.service.getAllCustomers().subscribe({
-      next: (data) => this.customers = data,
+      next: (data) => {
+        this.customers = data;
+        this.dataSource.data = data;
+      },
       error: (error) => console.log(error),
       complete: () => console.log("Requisição finalizada com sucesso")
     });
+  }
+
+  public applyFilters(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
